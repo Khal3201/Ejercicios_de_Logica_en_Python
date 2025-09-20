7e016ef306ef2f897b84788ad55e59c356ef9b65
khal3201 <khalrey320@gmail.com>
2025-09-20 13:38:50 -0600

    Ejercicio 4 agregado

---
+++ ejercicios1-10/Ej4_numerosPrimos.py
@@ -0,0 +1,20 @@
+'''
+
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+'''
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print("Números primos del 1 al 100:")
+for i in range(1, 101):
+    if es_primo(i):
+        print(i)
